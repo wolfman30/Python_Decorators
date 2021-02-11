55bd8b302258f6f906e9f15601d9c5d35e23b006
Andrew Wolf <wolfpassion20@gmail.com>
2021-02-11 17:16:21 -0500

    Added new folder tmp to contain text file written to by decorator

---
+++ python_docorators.py
@@ -0,0 +1,17 @@
+@log_message
+
+def a_function_that_returns_a_string():
+
+      return "A string"
+
+@log_message
+
+def a_function_that_returns_a_string_with_newline(s):
+
+      return "{}\n".format(s)
+
+@log_message
+
+def a_function_that_returns_another_string(string=""):
+
+            return "Another string"
