a6be35b8bfa66fe4145a75b0a35b3d7ccd518ffe
Andrew Wolf <wolfpassion20@gmail.com>
2021-02-21 21:35:43 -0500

    Added decorator function to file

--- python_docorators.py
+++ python_docorators.py
@@ -1,3 +1,11 @@
+def log_message(func):
+
+      def write_to_decorator_log(func):
+            with open('/tmp/decorator_logs.txt', 'w+') as decorator_log: 
+                  decorator_log.write(func)
+      return write_to_decorator_log
+
+
 @log_message
 
 def a_function_that_returns_a_string():
